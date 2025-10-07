94418fcf60e2c7aa33f80c2e0dbd57470cf2799c
Catherine Guelque <guelque@telecom-sudparis.eu>
2025-10-07 14:28:14 +0200

    [docker] Created separate Dockerfiles for GCC/Clang build and IntelOneApi build. The latter is 15Gb, ouch !

--- docker/build_docker_images.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker build -f Dockerfile_pallas_test . -t pallas/pallas_test:latest
--- docker/publish_docker_images.sh
+++ docker/publish_docker_images.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
+REGISTRY="registry.gitlab.inria.fr"
+PATH="pallas/pallas"
 
-docker login registry.gitlab.inria.fr
-docker build -t registry.gitlab.inria.fr/pallas/pallas -f Dockerfile_pallas_test .
-docker push registry.gitlab.inria.fr/pallas/pallas
+docker login "${REGISTRY}"
+
+docker build -f Dockerfile_pallas . -t "${PATH}":latest
+docker tag "${PATH}":latest "${REGISTRY}/${PATH}":latest
+docker push "${REGISTRY}/${PATH}":latest
+
+docker build -f Dockerfile_pallas_intel . -t "${PATH}"/intel:latest
+docker tag "${PATH}"/intel:latest "${REGISTRY}/${PATH}"/intel:latest
+docker push "${REGISTRY}/${PATH}"/intel:latest
