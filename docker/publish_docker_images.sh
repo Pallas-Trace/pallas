34fedf8b66feeb008d96e5dde67d34e4b6f845a8
Catherine Guelque <guelque@telecom-sudparis.eu>
2025-10-07 20:23:37 +0200

    [docker] Created custom IntelOneApi Dockerfile, to only test Pallas.

--- docker/publish_docker_images.sh
+++ docker/publish_docker_images.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
+
 REGISTRY="registry.gitlab.inria.fr"
-PATH="pallas/pallas"
+URI="pallas/pallas"
 
 docker login "${REGISTRY}"
 
-docker build -f Dockerfile_pallas . -t "${PATH}":latest
-docker tag "${PATH}":latest "${REGISTRY}/${PATH}":latest
-docker push "${REGISTRY}/${PATH}":latest
+#docker build -f Dockerfile_pallas . -t "${URI}":latest
+#docker tag "${URI}":latest "${REGISTRY}/${URI}":latest
+#docker push "${REGISTRY}/${URI}":latest
 
-docker build -f Dockerfile_pallas_intel . -t "${PATH}"/intel:latest
-docker tag "${PATH}"/intel:latest "${REGISTRY}/${PATH}"/intel:latest
-docker push "${REGISTRY}/${PATH}"/intel:latest
+docker build -f Dockerfile_pallas_intel . -t "${URI}"/intel:latest && \
+docker tag "${URI}"/intel:latest "${REGISTRY}/${URI}"/intel:latest && \
+docker push "${REGISTRY}/${URI}"/intel:latest
