d0a30da6085e9213c3e5d232a8dca64abd095ee8
Louis Delyon <louis.delyon+git@proton.me>
2025-12-23 11:50:23 +0100

    [fix] Fixed forgotten header change in pallas python

--- libraries/pallas_python/pallas_python.h
+++ libraries/pallas_python/pallas_python.h
@@ -11,8 +11,8 @@ namespace py = pybind11;
 
 #include <pallas/pallas.h>
 #include <pallas/pallas_archive.h>
-#include <pallas/pallas_storage.h>
 #include <pallas/pallas_record.h>
+#include <pallas/utils/pallas_storage.h>
 /* -*-
    mode: c;
    c-file-style: "k&r";
