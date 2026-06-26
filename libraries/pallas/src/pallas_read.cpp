56f1858117cd6e9982e1323bf92cbe5ef2d02e24
Louis Delyon <louis_delyon@telecom-sudparis.eu>
2026-06-26 16:59:38 +0200

    [fix] ThreadReader doesn't own the thread anymore

--- libraries/pallas/src/pallas_read.cpp
+++ libraries/pallas/src/pallas_read.cpp
@@ -666,10 +666,6 @@ void ThreadReader::loadCheckpoint(Cursor* checkpoint) {
 }
 
 ThreadReader::~ThreadReader() {
-    if (thread_trace && archive) {
-        pallas_log(DebugLevel::Debug, "Deleting ThreadReader %d\n", this->thread_trace->id);
-        archive->freeThread(this->thread_trace->id);
-    }
 }
 
 ThreadReader::ThreadReader(const ThreadReader& other) = default;
