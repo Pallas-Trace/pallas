d2f46a30bb39c4d2cdf6c7f615d1f1e55adc1a62
François Trahay <francois.trahay@telecom-sudparis.eu>
2026-09-17 10:31:28 +0200

    fix previous commit

--- libraries/pallas_python/python_read.cpp
+++ libraries/pallas_python/python_read.cpp
@@ -257,116 +257,106 @@ int get_read_flags_from_bools(bool enter_sequence, bool enter_loop) {
 }
 
 py::dict get_attributes(PyEvent& event, size_t occurrence) {
-    py::dict result;
-    pallas::Archive* archive = event.thread->archive;
-    auto* summary = event.self;
-    pallas::AttributeList* attribute_list;
-    if (event.self->attribute_buffer == nullptr)
-        return result;
-    if (summary->attribute_pos < summary->attribute_buffer_size) {
-        attribute_list = (pallas::AttributeList*)&summary->attribute_buffer[summary->attribute_pos];
-        while (attribute_list->index < occurrence) { /* move to the next attribute until we reach the needed index */
-            summary->attribute_pos += attribute_list->struct_size;
-            if (summary->attribute_pos >= summary->attribute_buffer_size)
-                return result;
-            attribute_list = (pallas::AttributeList*)&summary->attribute_buffer[summary->attribute_pos];
-        }
-        if (attribute_list->index > occurrence) {
-            pallas_error("Error fetching attribute %zu. We went too far (cur position: %d) !\n", occurrence, attribute_list->index);
-        }
-    } else {
-        return result;
-    byte* read_pos = summary->attribute_buffer;
-    attribute_list = (pallas::AttributeList*)read_pos;
-    while (attribute_list->index != occurrence) { /* move to the next attribute until we reach the needed index */
-        read_pos += attribute_list->struct_size;
-        if (read_pos > summary->attribute_buffer + summary->attribute_buffer_size) {
-            return result;
-        }
-        attribute_list = (pallas::AttributeList*)read_pos;
+  py::dict result;
+  pallas::Archive* archive = event.thread->archive;
+  auto* summary = event.self;
+  pallas::AttributeList* attribute_list;
+  if (event.self->attribute_buffer == nullptr)
+    return result;
+  if (summary->attribute_pos < summary->attribute_buffer_size) {
+    attribute_list = (pallas::AttributeList*)&summary->attribute_buffer[summary->attribute_pos];
+    while (attribute_list->index < occurrence) { /* move to the next attribute until we reach the needed index */
+      summary->attribute_pos += attribute_list->struct_size;
+      if (summary->attribute_pos >= summary->attribute_buffer_size)
+	return result;
+      attribute_list = (pallas::AttributeList*)&summary->attribute_buffer[summary->attribute_pos];
     }
     if (attribute_list->index > occurrence) {
-        pallas_error("Error fetching attribute %zu. We went too far (cur position: %d) !\n", occurrence, attribute_list->index);
+      pallas_error("Error fetching attribute %zu. We went too far (cur position: %d) !\n", occurrence, attribute_list->index);
     }
-    byte* reading_addr = (byte*)attribute_list->attributes;
-    byte* reading_end = (byte*)attribute_list + attribute_list->struct_size;
-    for (int i = 0; i < attribute_list->nb_values; i++) {
-        if (reading_addr >= reading_end)
-            break;
-        pallas::AttributeData* data = (pallas::AttributeData*)reading_addr;
-        uint16_t size = data->struct_size;
-        if (size == 0)
-            break;
-        const pallas::Attribute* attribute = archive->getAttribute(data->ref);
-        if (attribute) {
-            const pallas::String* name_str = archive->getString(attribute->name);
-            if (name_str) {
-                switch (attribute->type) {
-                case pallas::PALLAS_TYPE_NONE:
-                    break;
-                case pallas::PALLAS_TYPE_UINT8: {
-                    uint8_t v = data->value.uint8;
-                    result[name_str->str] = v;
-                    break;
-                }
-                case pallas::PALLAS_TYPE_UINT16: {
-                    uint16_t v = data->value.uint16;
-                    result[name_str->str] = v;
-                    break;
-                }
-                case pallas::PALLAS_TYPE_UINT32: {
-                    uint32_t v = data->value.uint32;
-                    result[name_str->str] = v;
-                    break;
-                }
-                case pallas::PALLAS_TYPE_UINT64: {
-                    uint64_t v = data->value.uint64;
-                    result[name_str->str] = v;
-                    break;
-                }
-                case pallas::PALLAS_TYPE_INT8: {
-                    int8_t v = data->value.int8;
-                    result[name_str->str] = v;
-                    break;
-                }
-                case pallas::PALLAS_TYPE_INT16: {
-                    int16_t v = data->value.int16;
-                    result[name_str->str] = v;
-                    break;
-                }
-                case pallas::PALLAS_TYPE_INT32: {
-                    int32_t v = data->value.int32;
-                    result[name_str->str] = v;
-                    break;
-                }
-                case pallas::PALLAS_TYPE_INT64: {
-                    int64_t v = data->value.int64;
-                    result[name_str->str] = v;
-                    break;
-                }
-                case pallas::PALLAS_TYPE_FLOAT: {
-                    float v = data->value.float32;
-                    result[name_str->str] = v;
-                    break;
-                }
-                case pallas::PALLAS_TYPE_DOUBLE: {
-                    double v = data->value.float64;
-                    result[name_str->str] = v;
-                    break;
-                }
-                case pallas::PALLAS_TYPE_STRING: {
-                    const pallas::String* val_str = archive->getString(data->value.string_ref);
-                    if (val_str)
-                        result[name_str->str] = val_str->str;
-                } break;
-                default:
-                    // TODO : Add more attribute types
-                    pallas_warn("Attributes of type %d are not yet handled in python\n", (int)attribute->type);
-                    break;
-                }
-            }
-        }
-        reading_addr += size;
-    }
+  } else {
     return result;
+  }
+   
+  byte* reading_addr = (byte*)attribute_list->attributes;
+  byte* reading_end = (byte*)attribute_list + attribute_list->struct_size;
+  for (int i = 0; i < attribute_list->nb_values; i++) {
+    if (reading_addr >= reading_end)
+      break;
+    pallas::AttributeData* data = (pallas::AttributeData*)reading_addr;
+    uint16_t size = data->struct_size;
+    if (size == 0)
+      break;
+    const pallas::Attribute* attribute = archive->getAttribute(data->ref);
+    if (attribute) {
+      const pallas::String* name_str = archive->getString(attribute->name);
+      if (name_str) {
+	switch (attribute->type) {
+	case pallas::PALLAS_TYPE_NONE:
+	  break;
+	case pallas::PALLAS_TYPE_UINT8: {
+	  uint8_t v = data->value.uint8;
+	  result[name_str->str] = v;
+	  break;
+	}
+	case pallas::PALLAS_TYPE_UINT16: {
+	  uint16_t v = data->value.uint16;
+	  result[name_str->str] = v;
+	  break;
+	}
+	case pallas::PALLAS_TYPE_UINT32: {
+	  uint32_t v = data->value.uint32;
+	  result[name_str->str] = v;
+	  break;
+	}
+	case pallas::PALLAS_TYPE_UINT64: {
+	  uint64_t v = data->value.uint64;
+	  result[name_str->str] = v;
+	  break;
+	}
+	case pallas::PALLAS_TYPE_INT8: {
+	  int8_t v = data->value.int8;
+	  result[name_str->str] = v;
+	  break;
+	}
+	case pallas::PALLAS_TYPE_INT16: {
+	  int16_t v = data->value.int16;
+	  result[name_str->str] = v;
+	  break;
+	}
+	case pallas::PALLAS_TYPE_INT32: {
+	  int32_t v = data->value.int32;
+	  result[name_str->str] = v;
+	  break;
+	}
+	case pallas::PALLAS_TYPE_INT64: {
+	  int64_t v = data->value.int64;
+	  result[name_str->str] = v;
+	  break;
+	}
+	case pallas::PALLAS_TYPE_FLOAT: {
+	  float v = data->value.float32;
+	  result[name_str->str] = v;
+	  break;
+	}
+	case pallas::PALLAS_TYPE_DOUBLE: {
+	  double v = data->value.float64;
+	  result[name_str->str] = v;
+	  break;
+	}
+	case pallas::PALLAS_TYPE_STRING: {
+	  const pallas::String* val_str = archive->getString(data->value.string_ref);
+	  if (val_str)
+	    result[name_str->str] = val_str->str;
+	} break;
+	default:
+	  // TODO : Add more attribute types
+	  pallas_warn("Attributes of type %d are not yet handled in python\n", (int)attribute->type);
+	  break;
+	}
+      }
+    }
+    reading_addr += size;
+  }
+  return result;
 }
